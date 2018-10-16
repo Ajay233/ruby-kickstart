087fa31aa467266b4f9cfd1f90d61a776cce677f
Ajay <ajaymungur@hotmail.com>
2018-10-16 13:40:45 +0000

    Session1 q7 first commit

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,9 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+  new_string = []
+  string.chars.each_with_index do |char, i| 
+    new_string << string[i+1] if char == "r" || char == "R"
+  end
+  new_string.join
 end
